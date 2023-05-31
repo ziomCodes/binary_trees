ceb93e5704cf8f8a57eaee4f42ed2a50cab57fa0
ziomCodes <ziomcode@gmail.com>
2023-05-31 12:12:16 +0000

    updated binary

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,47 +1,35 @@
 #include "binary_trees.h"
+#include "limits.h"
 
 /**
- * binary_search_tree_checker - Checks if a binary tree is \
- * a binary search tree.
- * @tree: The binary tree.
- * @min: The minimum value of the tree.
- * @max: The mazimum value of the tree.
- * @is_bst: A pointer to the tree's validation flag.
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
+ *
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
-void binary_search_tree_checker(const binary_tree_t *tree,
-	int min, int max, int *is_bst)
+int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
 	if (tree != NULL)
 	{
-		if ((tree->n > min) && (tree->n < max))
-		{
-			binary_search_tree_checker(tree->left, min, tree->n, is_bst);
-			binary_search_tree_checker(tree->right, tree->n, max, is_bst);
-		}
-		else
-		{
-			if (is_bst != NULL)
-			{
-				*is_bst = 0;
-			}
-		}
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, hi));
 	}
+	return (1);
 }
 
 /**
- * binary_tree_is_bst - Checks if a binary tree is binary search tree.
- * @tree: The binary tree.
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
  *
- * Return: The 1 if the binary tree is binary search tee, otherwise 0.
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int is_bst = 0;
-
-	if (tree != NULL)
-	{
-		is_bst = 1;
-		binary_search_tree_checker(tree, INT_MIN, INT_MAX, &is_bst);
-	}
-	return (is_bst);
+	if (tree == NULL)
+		return (0);
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
